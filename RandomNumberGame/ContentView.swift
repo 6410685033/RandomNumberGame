9efac30409f937bd838b9cd0d0e2bb32d48463cc
Pond <6410685033@student.tu.ac.th>
2024-01-19 14:09:27 +0700

    Initial Commit

---
+++ RandomNumberGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RandomNumberGame
+//
+//  Created by Thammasat Thonggamgaew on 19/1/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RandomNumberGame/RandomNumberGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomNumberGameApp.swift
+//  RandomNumberGame
+//
+//  Created by Thammasat Thonggamgaew on 19/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct RandomNumberGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
