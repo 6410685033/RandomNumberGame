7ce318a9fcdea57c30848cc8c60ea0117843a2a8
Pond <6410685033@student.tu.ac.th>
2024-01-19 15:47:11 +0700

    Complete Random Number v1.0

--- RandomNumberGame/ContentView.swift
+++ RandomNumberGame/ContentView.swift
@@ -8,12 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var game = Game()
+    @State var guessNum: Double = 0.0
+    @State var showResult = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+                Text("Random Number Game")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                            .foregroundColor(.primary)
+                            .shadow(color: .gray, radius: 5, x: 0, y: 5)
+                    )
+                
+            Spacer()
+            
+            VStack {
+                Text("Guess a number :)")
+                    .font(.headline)
+                Text("Round \(game.round)")
+            }.padding()
+            
+            Text(
+                String(Game.value(num: guessNum)))
+                .font(.largeTitle)
+            
+            HStack {
+                Text("0")
+                Slider(value: $guessNum)
+                Text("100")
+            }
+            
+            Button("Guess") {
+                showResult = true
+                game.check(guess: guessNum)
+            }.padding()
+            .alert(isPresented: $showResult) {
+                Alert(
+                    title: Text("Result"),
+                    message: Text(game.msg),
+                    dismissButton: .default(Text("OK")) {
+                        // reset guess, new game
+                        if game.win {
+                            guessNum = 0.0
+                            game.startNewGame()
+                        }
+                        
+                    }
+                )
+            }
+            Spacer()
         }
         .padding()
     }
@@ -22,3 +71,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
---
+++ RandomNumberGame/Models/Game.swift
@@ -0,0 +1,42 @@
+//
+//  Game.swift
+//  RandomNumberGame
+//
+//  Created by Thammasat Thonggamgaew on 19/1/2567 BE.
+//
+
+import Foundation
+
+struct Game {
+    var target = Double.random(in: 0...1)
+    var round = 0
+    var win = false
+    var msg = ""
+    
+    // "mutating" use to inform that func edit field
+    mutating func check(guess: Double) {
+        let guessNum = lround(guess*100)
+        let check = lround(target*100)
+        if guessNum == check {
+            msg = "Correct!"
+            win = true
+        } else if guessNum < check {
+            msg = "Higher!"
+            round += 1
+        } else {
+            msg = "Lower!"
+            round += 1
+        }
+    return
+    }
+    
+    mutating func startNewGame() {
+        round = 0
+        target = Double.random(in: 0...1)
+    }
+    
+    static func value(num: Double) -> Int{
+        return lround(num*100)
+    }
+
+}
